33c3642d575f2db12087859ff3b61fec54c7b5e2
Pravin sahu <pravin.sahu.wrk@gmail.com>
2024-08-16 23:27:47 +0530

    update

--- src/components/TimelineOnline.tsx
+++ src/components/TimelineOnline.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import timelineonline from "public/timeline.svg";
+import newtimeline from "public/InauguralCeremony.png"
 export default function TimelineOnline() {
   return (
     <div id="timeline" className="flex flex-col items-center justify-center w-full my-8">
       <h1 className="text-white text-center text-5xl font-bold px-10">Timeline</h1>
       <div className="flex h-1/2">
-        <Image src={timelineonline} alt="timeline" className="w-full" width={1000} height={1000} />
+        <Image src={newtimeline} alt="timeline" className="w-full" width={1000} height={1000} />
       </div>
     </div>
   );
